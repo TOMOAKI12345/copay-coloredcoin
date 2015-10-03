2db87a12100026a47486b2c5d1fa5f18949e667f
TOMOAKI12345 <tomoaki12345@gmail.com>
2015-10-03 18:38:20 +0900

    gitignore

--- src/js/services/logHeader.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('copayApp.services')
-  .factory('logHeader', function($log, isChromeApp, isCordova, nodeWebkit) {
-    $log.info('Starting Copay v' + window.version + ' #' + window.commitHash);
-    $log.info('Client: isCordova:', isCordova, 'isChromeApp:', isChromeApp, 'isNodeWebkit:', nodeWebkit.isDefined());
-    $log.info('Navigator:', navigator.userAgent);
-    return {};
-  });
